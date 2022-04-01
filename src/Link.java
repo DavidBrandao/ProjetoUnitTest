f57d46506ddf9f98176c736e2e54318168453a39
David Brandao <davfb.ferreira@gmail.com>
2022-04-01 01:43:49 -0300

    Correção de bugs na classe Link.java e adição de casos de teste

--- src/Link.java
+++ src/Link.java
@@ -27,8 +27,9 @@ public class Link implements Comparable<Link> {
   }
 
   /* retorna o tamanho de um link entre duas cidades */
+  //[Bug] Modificado o retorno de (cidade1.distancia) para a variável tamanho
   public int getTamanho() {
-    return cidade1.distancia;
+    return tamanho;
   }
 
   /* retorna true se o link entre duas cidades existir */
@@ -37,32 +38,36 @@ public class Link implements Comparable<Link> {
   }
 
   /* seta usado como true ou false a depender de o link ser criado ou não */
-  public void setUsado(boolean u) {
-    usado = false;
+  public void setUsado(boolean estado) {
+    usado = estado;
   }
+
+  //[Ajuste] Modificado nome da variável u -> estado
+  //[Bug] Atribução da variavel usado para a variavel do metodo (Codigo estava com "false" hardcoded)
 
   /* returna uma string com a descrição de um Link entre cidades.
    * exemplo Link formado por Cidade1, tamanho 3 e Cidade2, retorna "Cidade1 3 Cidade2".
    */
   public String toString() {
-    return cidade1.toString() + " " + cidade2.toString();
+    return cidade1.toString() + " " + tamanho + " " + cidade2.toString();
   }
+  //[BUG] Ajustando o código para concatenar a variável tamanho, como informado na descrição
 
   /* Compara dois links para saber o que eles tem em comum
-   * returna 0 se os links tiverem as mesmas cidade1 e cidade2
-   * returna negativo se this.cidade1 < l.cidade1 (Alfabetcamente)
+   * retorna 0 se os links tiverem as mesmas cidade1 e cidade2
+   * retorna negativo se this.cidade1 < l.cidade1 (Alfabetcamente)
    * retorna negativo se cidade1 é igual nos dois links
    * retorna negativo se this.cidade2 < l.cidade2 (Alfabetcamente)
    * retorna positivo caso contrário
    * Exemplo: Link1: Recife 3 Olinda / Link2: Recife 2 Camaragibe
    * Link1.compareTo(Link2) -> Resultado: retorna negativo, porque cidade1 é Recife e é igual nos dois Links
    */
+  //[Bug] Foi Corrigido o segundo termo condicional do else IF, mudando de AND (&&) para OR (||)
   public int compareTo(Link l) {
 
     if(cidade1==l.cidade1 && cidade2==l.cidade2)
       return 0;
-    else if((cidade1.comparaNome(l.cidade1)<0)
-            || (cidade1==l.cidade1 && cidade2.comparaNome(l.cidade2)<0))
+    else if((cidade1.comparaNome(l.cidade1)<0)  || (cidade1==l.cidade1) || cidade2.comparaNome(l.cidade2)<0)
       return -1;
     else
       return +1;
--- test/LinkTest.java
+++ test/LinkTest.java
@@ -1,26 +1,129 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkTest {
 
-    @Test
-    void getTamanho() {
+    static Cidade cidade1 = new Cidade("Primeira Cidade");
+    static Cidade cidade2 = new Cidade("Segunda Cidade");
+    static Cidade cidade3 = new Cidade("Terceira Cidade");
+    static int tamanho = 10;
+    static int tamanhoNegativo = -10;
+    static int tamanhoZero = 0;
+    static String stringDesejadaLink = "Amegakure 10 Konoha";
+    static String stringDesejadaLinkInvertido = "Konoha 10 Amegakure";
+
+    @BeforeAll
+    public static void popularCidades(){
+        cidade1.nome = "Konoha";
+        cidade1.distancia = 100;
+        cidade2.nome = "Amegakure";
+        cidade2.distancia = 50;
+        cidade3.nome = "Miyuki";
+        cidade3.distancia = 50;
     }
 
     @Test
-    void isUsado() {
+    void testGetTamanhoPositivo() {
+        Link link = new Link(cidade1, cidade2, tamanho);
+        assertEquals(tamanho, link.getTamanho());
     }
 
     @Test
-    void setUsado() {
+    void testGetTamanhoNegativo() {
+        Link link = new Link(cidade1, cidade2, tamanhoNegativo);
+        assertEquals(tamanhoNegativo, link.getTamanho());
+    }
+
+    @Test
+    void testGetTamanhoZerado() {
+        Link link = new Link(cidade1, cidade2, tamanhoZero);
+        assertEquals(tamanhoZero, link.getTamanho());
+    }
+
+    @Test
+    void testIsUsadoTrue() {
+        Link link = new Link(cidade1, cidade2, tamanho);
+        assertTrue(link.isUsado());
+    }
+
+    @Test
+    void testIsUsadoFalse(){
+        Link link = new Link(cidade1, cidade2, tamanho);
+        link.usado = false;
+        assertFalse(link.isUsado());
+    }
+
+    @Test
+    void testSetUsadoFalse() {
+        Link link = new Link(cidade1, cidade2, tamanho);
+        link.setUsado(false);
+        assertFalse(link.isUsado());
+    }
+
+    @Test
+    void testSetUsadoTrue(){
+        Link link = new Link(cidade1, cidade2, tamanho);
+        link.usado = false;
+        link.setUsado(true);
+        assertTrue(link.isUsado());
     }
 
     @Test
     void testToString() {
+        Link link = new Link(cidade1, cidade2, tamanho);
+        assertEquals(stringDesejadaLink, link.toString());
     }
 
     @Test
-    void compareTo() {
+    void testToStringInvertido(){
+        Link link = new Link(cidade1, cidade2, tamanho);
+        assertNotEquals(stringDesejadaLinkInvertido, link.toString());
     }
+
+    //retorna 0 se os links tiverem as mesmas cidade1 e cidade2
+    @Test
+    void testCompareToCidadesIguais() {
+        Link link = new Link(cidade1, cidade2, tamanho);
+        Link link2 = new Link(cidade1, cidade2, tamanho);
+        assertEquals(0, link.compareTo(link2));
+    }
+
+    //retorna negativo se this.cidade1 < l.cidade1 (Alfabetcamente)
+    @Test
+    void testCompareToCidadesDiferentesNegativo() {
+        Link link = new Link(cidade1, cidade2, tamanho);
+        Link link2 = new Link(cidade1, cidade1, tamanho);
+        System.out.println(link.compareTo(link2));
+        assertEquals(-1, link.compareTo(link2));
+    }
+
+    //retorna negativo se as cidades um são iguais
+    @Test
+    void testCompareToCidade1Iguais() {
+        Link link = new Link(cidade3, cidade2, tamanho);
+        Link link2 = new Link(cidade1, cidade2, tamanho);
+        System.out.println(link.compareTo(link2));
+        assertEquals(-1, link.compareTo(link2));
+    }
+
+    //retorna negativo se this.cidade2 < l.cidade2 (Alfabetcamente)
+    @Test
+    void testCompareToCidadesIguaisNegativo(){
+        Link link = new Link(cidade1, cidade2, tamanho);
+        Link link2 = new Link(cidade3, cidade2, tamanho);
+        System.out.println(link.cidade2);
+        System.out.println(link2.cidade2);
+        assertEquals(-1, link.compareTo(link2));
+    }
+
+    //retorna positivo se this.cidade2 > l.cidade2 (Alfabetcamente)
+    @Test
+    void testCompareToCidadesDiferentesPositivo(){
+        Link link = new Link(cidade1, cidade3, tamanho);
+        Link link2 = new Link(cidade2, cidade1, tamanho);
+        assertEquals(1, link.compareTo(link2));
+    }
+
 }
